2f5dcd383f4c5a9633442e51ab0ecd536e3abff4
Gabriel Eric <gabriel.eric@ufrpe.br>
2025-08-15 23:23:02 -0300

    init project

---
+++ src/main/java/com/example/buscativa/BuscativaApplication.java
@@ -0,0 +1,13 @@
+package com.example.buscativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/buscativa/BuscativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buscativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
